7cbf000510af0409443e448d89744625460f962b
[rezaeiar] <rezaeiar0@gmail.com>
2024-05-26 13:01:52 +0430

    Customize the container

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,11 @@ const config: Config = {
         "./app/**/*.{js,ts,jsx,tsx,mdx}",
     ],
     theme: {
+        container: {
+            center: true,
+            padding: '1rem',
+            screens: {}
+        },
         extend: {
         },
     },

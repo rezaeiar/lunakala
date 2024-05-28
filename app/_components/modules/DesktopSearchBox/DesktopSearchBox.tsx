a640f9c9ded9ce09d7c1f1bf5352faccd9474ddc
[rezaeiar] <rezaeiar0@gmail.com>
2024-05-28 19:11:11 +0430

    feat: Responsive NavigationBar components and DesktopSearchBox development

--- app/_components/modules/DesktopSearchBox/DesktopSearchBox.tsx
+++ app/_components/modules/DesktopSearchBox/DesktopSearchBox.tsx
@@ -5,20 +5,23 @@ export default function DesktopSearchBox() {
 
     const [isFoucs, setIsFoucs] = useState(false)
     return (
-        <div className="before:content-[''] before:absolute before:h-full before:w-full before:bg-gray-500/20 before:top-0 before:left-0">
-            <div className="flex bg-gray-100 w-[500px] rounded-md h-12">
+        <div className={`${isFoucs ? 'before:visible before:opacity-100' : 'before:invisible before:opacity-0'} before:content-[''] before:absolute before:transition-all before:h-full before:w-full before:bg-black/20 before:top-0 before:left-0 before:z-10`}>
+            <div className={`${isFoucs ? 'bg-white': 'bg-gray-100'} transition-colors flex w-[360px] lg:w-[540px] rounded-md h-10 lg:h-12 relative z-30`}>
                 <div className="h-full aspect-square flex items-center justify-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 text-gray-400">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5 lg:size-6 text-gray-400">
                         <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                     </svg>
                 </div>
                 <input
                     type="text"
-                    className='h-full grow bg-transparent outline-none font-dana text-sm text-gray-400 z-40'
+                    className='h-full grow bg-transparent outline-none font-dana text-gray-400 z-40 text-xs lg:text-sm'
                     onFocus={() => setIsFoucs(true)}
                     onBlur={() => setIsFoucs(false)}
                     placeholder='محصول، برند یا دسته بندی خود را جستجو کنید'
                 />
+                <div className="h-20 w-full absolute top-14 bg-white cursor-pointer z-30 rounded-md">
+
+                </div>
             </div>
         </div>
     )
--- app/_components/modules/NavigationBar/NavigationBar.tsx
+++ app/_components/modules/NavigationBar/NavigationBar.tsx
@@ -8,7 +8,12 @@ export default function NavigationBar() {
             <div className="container">
                 <div className="flex flex-col">
                     <div className="flex justify-between items-center">
-                        <div className="flex gap-8">
+                        <div className="flex gap-6 lg:gap-8">
+                            <div className="flex xl:hidden items-center text-gray-600">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                                </svg>
+                            </div>
                             <Link href="/" className="flex items-center gap-2 font-bold font-morabba">
                                 <div className="flex h-10 w-10 rounded-md bg-primary text-white text-lg items-center justify-center">
                                     ل
@@ -24,21 +29,19 @@ export default function NavigationBar() {
                             </Link>
                             <DesktopSearchBox />
                         </div>
-                        <div className="flex gap-6">
+                        <div className="flex gap-4 lg:gap-6">
                             <Link href='/account/login' className="flex divide-x-reverse divide-gray-900 rounded-md border border-gray-300 font-dana">
-                                <div className="my-1.5 px-4 text-gray-800 border-l border-gray-800">
+                                <div className="my-1.5 px-4 text-gray-800 border-l border-gray-800 text-sm lg:text-base">
                                     ورود
                                 </div>
-                                <div className="my-1.5 px-4 text-gray-800">
+                                <div className="my-1.5 px-4 text-gray-800 text-sm lg:text-base">
                                     ثبت نام
                                 </div>
                             </Link>
-                            <Link href='' className='relative p-1.5 aspect-square flex items-center justify-center border border-gray-300 rounded-md'>
+                            <Link href='' className='relative p-1 lg:p-1.5 aspect-square flex items-center justify-center border border-gray-300 rounded-md'>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 text-gray-800">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
                                 </svg>
-
-
                             </Link>
                         </div>
                     </div>

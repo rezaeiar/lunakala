aad33e569cd72e1f46639a486d92c367b128c7ba
[rezaeiar] <rezaeiar0@gmail.com>
2024-05-29 01:01:45 +0430

    feat: Development of Mobile Search Box and Mobile Navigation Bar

---
+++ app/_components/modules/MobileSearchBox/MobileSearchBox.tsx
@@ -0,0 +1,25 @@
+"use client"
+import React, { useState } from 'react'
+
+export default function MobileSearchBox() {
+
+    const [isFoucs, setIsFoucs] = useState(false)
+    return (
+        <div className={`block md:hidden grow`}>
+            <div className={`bg-gray-100 transition-colors flex rounded-md h-10 relative z-30 w-full`}>
+                <div className="h-full aspect-square flex items-center justify-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5 text-gray-400">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                    </svg>
+                </div>
+                <input
+                    type="text"
+                    className='h-full grow bg-transparent outline-none font-dana text-gray-400 z-40 text-xs'
+                    onFocus={() => setIsFoucs(true)}
+                    onBlur={() => setIsFoucs(false)}
+                    placeholder='محصول، برند یا دسته بندی خود را جستجو کنید'
+                />
+            </div>
+        </div>
+    )
+}
--- app/_components/modules/NavigationBar/NavigationBar.tsx
+++ app/_components/modules/NavigationBar/NavigationBar.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Link from 'next/link'
 import DesktopSearchBox from '../DesktopSearchBox/DesktopSearchBox'
+import MobileSearchBox from '../MobileSearchBox/MobileSearchBox'
 
 export default function NavigationBar() {
     return (
         <div className='pt-3 md:pt-4'>
             <div className="container">
-                <div className="flex flex-col">
+                <div className="flex flex-col gap-3">
                     <div className="flex justify-between items-center">
                         <div className="flex justify-between md:justify-start w-full md:w-auto gap-6 lg:gap-8">
                             <div className="flex xl:hidden items-center text-gray-600">
@@ -52,6 +53,14 @@ export default function NavigationBar() {
                                 </svg>
                             </Link>
                         </div>
+                    </div>
+                    <div className="flex md:hidden items-center gap-6">
+                        <MobileSearchBox />
+                        <Link href='/cart'>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5 text-gray-800">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                            </svg>
+                        </Link>
                     </div>
                 </div>
             </div>

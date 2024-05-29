42c65e814a617e4650acb3d1ea857a59b6d10e24
[rezaeiar] <rezaeiar0@gmail.com>
2024-05-29 23:19:12 +0430

    feat: The start of the development of the mobile search box when focusing

--- app/_components/modules/MobileSearchBox/MobileSearchBox.tsx
+++ app/_components/modules/MobileSearchBox/MobileSearchBox.tsx
@@ -3,9 +3,9 @@ import React, { useState } from 'react'
 
 export default function MobileSearchBox() {
 
-    const [isFoucs, setIsFoucs] = useState(false)
+    const [isFoucs, setIsFoucs] = useState(true)
     return (
-        <div className={`block md:hidden grow`}>
+        <div className={`${isFoucs ? "bg-white fixed top-0 left-0 w-full h-full p-4": ''} transition-all block md:hidden grow`}>
             <div className={`bg-gray-100 transition-colors flex rounded-md h-10 relative z-30 w-full`}>
                 <div className="h-full aspect-square flex items-center justify-center">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5 text-gray-400">

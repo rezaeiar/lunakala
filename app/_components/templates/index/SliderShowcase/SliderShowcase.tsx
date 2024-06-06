f17178270e05d88df92e96b7f4ad5dcc574457cd
[rezaeiar] <rezaeiar0@gmail.com>
2024-06-06 18:09:47 +0430

    feat: Development of navigation and pagination of the main index page slider

--- app/_components/templates/index/SliderShowcase/SliderShowcase.tsx
+++ app/_components/templates/index/SliderShowcase/SliderShowcase.tsx
@@ -3,33 +3,69 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper/modules';
+import 'swiper/css/effect-fade';
+import { Autoplay, Pagination, Navigation, EffectFade } from 'swiper/modules';
 import Image from 'next/image';
-
 
 export default function SliderShowcase() {
     return (
-        <div className="mb-6">
-            <div className="">
-                <div className="new-releases-prev">
-                    l
+        <div className="mb-6 relative select-none group">
+            <div className="absolute bottom-10 right-10 invisible opacity-0 flex transition-all gap-2 items-center z-20 group-hover:visible group-hover:opacity-100">
+                <div className="new-releases-prev border-gray-400 bg-gray-100 rounded-full h-10 w-10 flex items-center justify-center cursor-pointer">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5 text-gray-600">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+                    </svg>
                 </div>
-                <div className="new-releases-prev">
-                    r
+                <div className="new-releases-next border-gray-400 bg-gray-100 rounded-full h-10 w-10 flex items-center justify-center cursor-pointer">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5 text-gray-600">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+                    </svg>
                 </div>
             </div>
+            <div id="showcase-pagination" className='z-20 absolute bottom-0 w-full h-10 flex items-center gap-2 justify-center'></div>
             <Swiper
-                cssMode={true}
-                pagination={true}
-                mousewheel={true}
-                keyboard={true}
-                modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+                modules={[Navigation, Pagination, Autoplay, EffectFade]}
+                autoplay={{
+                    delay: 2500,
+                    disableOnInteraction: false,
+                }}
+                pagination={{
+                    el: "#showcase-pagination",
+                    type: "bullets",
+                    bulletClass: "h-1.5 w-1.5 bg-gray-900/50 rounded-full block cursor-pointer transition-all",
+                    bulletActiveClass: "!bg-white w-3",
+                    clickable: true,
+                }}
+                loop={true}
+                effect='fade'
                 className="mySwiper"
                 navigation={{
                     prevEl: '.new-releases-prev',
                     nextEl: '.new-releases-next',
                 }}
             >
+                <SwiperSlide>
+                    <div className="">
+                        <Image
+                            src={'/images/banner/b2.webp'}
+                            alt='festival-banner'
+                            width={5000}
+                            height={5000}
+                            className='w-full h-96 object-cover'
+                        />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="">
+                        <Image
+                            src={'/images/banner/b1.webp'}
+                            alt='festival-banner'
+                            width={5000}
+                            height={5000}
+                            className='w-full h-96 object-cover'
+                        />
+                    </div>
+                </SwiperSlide>
                 <SwiperSlide>
                     <div className="">
                         <Image

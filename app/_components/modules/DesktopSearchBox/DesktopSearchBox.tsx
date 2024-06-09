679598762983b74b9f1e5ede6e5ec39271e98bca
[rezaeiar] <rezaeiar0@gmail.com>
2024-06-09 13:15:45 +0430

    fix: Fix header overlay bug

--- app/_components/modules/DesktopSearchBox/DesktopSearchBox.tsx
+++ app/_components/modules/DesktopSearchBox/DesktopSearchBox.tsx
@@ -7,7 +7,7 @@ export default function DesktopSearchBox() {
     const [isFoucs, setIsFoucs] = useState(false)
     const [searchedValue, setSearchedValue] = useState("")
     return (
-        <div className={`${isFoucs ? 'before:visible before:opacity-100' : 'before:invisible before:opacity-0'} before:content-[''] before:absolute before:transition-all before:h-full before:w-full before:bg-black/20 before:top-0 before:left-0 before:z-10 hidden md:block`}>
+        <div className={`${isFoucs ? 'before:visible before:opacity-100' : 'before:invisible before:opacity-0'} before:content-[''] before:fixed before:transition-all before:h-full before:w-full before:bg-black/20 before:top-0 before:left-0 before:z-10 hidden md:block`}>
             <div className={`${isFoucs ? 'bg-white' : 'bg-gray-100'} transition-colors flex w-[340px] lg:w-[540px] rounded-md h-10 lg:h-12 relative z-30`}>
                 <div className="h-full aspect-square flex items-center justify-center">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5 lg:size-6 text-gray-400">

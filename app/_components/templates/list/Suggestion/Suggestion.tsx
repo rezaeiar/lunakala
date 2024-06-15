831ebfc25f697119e81955bdb44b5b0886128327
[rezaeiar] <rezaeiar0@gmail.com>
2024-06-15 16:20:20 +0430

    feat: Development of suggestion component for list page

--- app/_components/templates/list/Suggestion/Suggestion.tsx
+++ app/_components/templates/list/Suggestion/Suggestion.tsx
@@ -4,8 +4,19 @@ export default function Suggestion() {
     return (
         <div>
             <div className="container">
-                <div className="flex items-center gap-4 text-gray-400 font-dana py-4">
-                    
+                <div className="flex items-center gap-4 text-gray-500 font-dana pb-4 text-xs">
+                    <div className="py-1.5 px-6 rounded-md border bg-white border-gray-300">
+                        اپل واچ se
+                    </div>
+                    <div className="py-1.5 px-6 rounded-md border bg-white border-gray-300">
+                        اپل واچ se
+                    </div>
+                    <div className="py-1.5 px-6 rounded-md border bg-white border-gray-300">
+                        اپل واچ se
+                    </div>
+                    <div className="py-1.5 px-6 rounded-md border bg-white border-gray-300">
+                        اپل واچ se
+                    </div>
                 </div>
             </div>
         </div>
